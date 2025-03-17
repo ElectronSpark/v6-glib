fadf9c097d016aa8fc21e3cc0945080620bdd385
Florian "sp1rit"​ <sp1rit@disroot.org>
2025-03-17 13:06:40 +0100

    gio/java+android: Added implementatation for AndroidContentFile
    This patch introduces GFile{Input,Output}Stream wrappers for their
    corresponding Java counterparts in GIO and uses those in order to
    introduce a GFile implementation for Androids content:// URIs.
    
    The test cases should probably be extended (esp. async tests should be
    added, given that glib will spawn a new thread for them), but I don't
    like writing test cases 🙃.

--- gio/giomodule.c
+++ gio/giomodule.c
@@ -72,6 +72,10 @@
 #include <AvailabilityMacros.h>
 #endif
 
+#ifdef G_PLATFORM_ANDROID
+#include "gandroidcontentfileprivate.h"
+#endif // G_PLATFORM_ANDROID
+
 #define __GLIB_H_INSIDE__
 #include "gconstructor.h"
 #undef __GLIB_H_INSIDE__
@@ -1387,6 +1391,9 @@ _g_io_modules_ensure_loaded (void)
 #ifdef G_OS_WIN32
       g_type_ensure (_g_win32_network_monitor_get_type ());
 #endif
+#ifdef G_PLATFORM_ANDROID
+      g_type_ensure (g_android_vfs_get_type ());
+#endif // G_PLATFORM_ANDROID
 
       g_once_init_leave (&loaded_dirs, TRUE);
     }
